d4d3b4f11bce7562d6a895d0504fca6040b9b2df
Keith MacDonald <macdonaldkeith@hotmail.com>
2020-08-06 14:06:20 -0400

    variable port

--- app.js
+++ app.js
@@ -60,4 +60,6 @@ app.use('/users', require('./routes/users'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server started on PORT: ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server started on PORT: ${PORT}`);
+});

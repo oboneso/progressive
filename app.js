08aced3fc04cad2e51f4629a921f3a7d452d5d89
Keith MacDonald <macdonaldkeith@hotmail.com>
2020-08-10 22:10:21 -0400

    blog and youtube functionality added

--- app.js
+++ app.js
@@ -116,7 +116,7 @@ app.get('/oauth2callback', (req, res) => {
 
 app.get('/.well-known/pki-validation/CE280DC890098AC71F3E31E297C7272F.txt', (req, res) => {
   console.log(req.body)
-  res.send('This is the ssl page')
+  res.send('CE280DC890098AC71F3E31E297C7272F.txt')
 })
 
 
